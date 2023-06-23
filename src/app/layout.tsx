42e41e9620d4789aec9d751a6586edf41ab59c78
RuslanDakeshev <bil08@outlook.com>
2023-06-23 02:05:52 +0200

    header/fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.scss';
 import { Inter } from 'next/font/google';
 import { Header } from '../components/Header/Header';
 
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -13,8 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='uk'>
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
         <Header />
+        {children}
       </body>
       
     </html>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,13 @@
-import Link from "next/link";
+// import Link from "next/link";
+
+'use client'
+
 import styles from './Header.module.scss'
 import classNames from 'classnames';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState  } from 'react';
+import Image from "next/image";
+import { Button } from '../Button/Button'
+
 
 type Props = {
   children: ReactNode;
@@ -9,10 +15,25 @@ type Props = {
 };
 
 const Header = ({ children, className }: Props) => {
+  const [screenWidth, setScreenWidth] = useState<number>(0);
+ useEffect(() => {
+    setScreenWidth(window.innerWidth);
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
     const classes = classNames(styles.header,styles.container, className);
-    return (
-        <header className={classes}>ddd{children}
-            
+  
+  return (
+      <header className={classes}>
+      <div className={classNames(styles.headerContainer, { [styles.largeScreen]: screenWidth > 768 })}>
+        <Image src="/path431.png" width={64} height={54} alt="Logo" className={classNames(styles.logo)}/>
+        <div className={classNames(styles.buttonContainer)}>
+        {screenWidth > 768 && <a href="#">Про проєкт</a>}
+          <Button />
+          </div>
+      </div> 
+      {children}
         </header>
     )
 }
