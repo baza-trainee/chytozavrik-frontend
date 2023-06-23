a4b7039fa930da95c2dbd654739469be8f8e3073
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-06-23 10:22:48 +0300

    Add common BUtton, prittierrc.json

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,19 +1,22 @@
-import { Container, Typography } from 'components/common';
+import { Button, Container, Typography } from 'components/common';
 import styles from './Hero.module.scss';
 
 const Hero = () => {
   return (
     <section className={styles.section}>
       <Container className={styles.container}>
-        <Typography component='h1' variant='h1' className={styles.title}>
+        <Typography component="h1" variant="h1" className={styles.title}>
           Розпочни свою велику книжкову пригоду!
         </Typography>
-        <Typography component='p' variant='body' className={styles.text}>
+        <Typography component="p" variant="body" className={styles.text}>
           Інтерактивна вікторина для маленьких книголюбів, яка зробить процес читання ще цікавішим
         </Typography>
-        <button className={styles.button} type='button'>
+        {/* <button className={styles.button} type="button">
           Почати гру
-        </button>
+        </button> */}
+        <Button color="secondary" className={styles.button}>
+          Почати гру
+        </Button>
       </Container>
     </section>
   );
---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,38 @@
+import { MouseEventHandler, ReactNode } from 'react';
+import styles from './Button.module.scss';
+import classNames from 'classnames';
+
+type Props = {
+  children: ReactNode;
+  type?: HTMLButtonElement['type'];
+  className?: string;
+  variant?: 'filled' | 'outline' | 'text';
+  color?: 'primary' | 'secondary' | 'tertiary' | 'quaternary' | 'error';
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+};
+
+const Button = ({
+  children,
+  type = 'button',
+  onClick,
+  className,
+  variant = 'filled',
+  color = 'primary',
+}: Props) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={classNames(
+        className,
+        styles.button,
+        styles[`button--${variant}`],
+        styles[`button--${color}`]
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
