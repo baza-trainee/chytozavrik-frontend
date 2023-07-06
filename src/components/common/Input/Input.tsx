e882f7c6b3c5fe3fedce71a0a891e00de07a8625
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-07-06 20:17:19 +0300

    ADD: Input common component.
    ADD: Input to dialog.
    
    ADD: react hook forms and yup libs.

---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,45 @@
+import { InputHTMLAttributes, useMemo } from 'react';
+import { useController, UseControllerProps, FieldValues } from 'react-hook-form';
+import styles from './Input.module.scss';
+
+export type InputProps<T extends FieldValues> = InputHTMLAttributes<HTMLInputElement> &
+  UseControllerProps<T> & {
+    label?: string;
+  };
+
+export type InputStatus = 'normal' | 'filled' | 'error';
+
+const Input = <T extends FieldValues>({ label, name, control, ...props }: InputProps<T>) => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController<T>({ name, control });
+
+  const status = useMemo<InputStatus>(() => {
+    if (error) {
+      return 'error';
+    } else if (field.value.length > 0) {
+      return 'filled';
+    }
+    return 'normal';
+  }, [error, field.value]);
+
+  return (
+    <div className={styles.group} data-status={status}>
+      <label className={styles.label}>
+        {label && <span className={styles['label-text']}>{label}</span>}
+        <span className={styles['input-group']}>
+          <input
+            className={styles.input}
+            {...field}
+            value={field.value === 0 ? '' : field.value}
+            {...props}
+          />
+        </span>
+        {error && <span className={styles.message}>{error.message}</span>}
+      </label>
+    </div>
+  );
+};
+
+export default Input;
--- src/components/modals/DonateDialog/DonateDialog.tsx
+++ src/components/modals/DonateDialog/DonateDialog.tsx
@@ -1,16 +1,63 @@
-import { Typography } from '@/components/common';
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { Button, Typography } from '@/components/common';
 import styles from './DonateDialog.module.scss';
+import Input from '@/components/common/Input';
 
 type Props = {
   onClose: () => void;
 };
 
+const schema = yup.object({
+  donate: yup.number().positive().required(),
+});
+
+type FormData = yup.InferType<typeof schema>;
+
+const defaultValues: FormData = {
+  donate: 0,
+};
+
 const DonateDialog = ({ onClose }: Props) => {
+  const { control, handleSubmit } = useForm({
+    defaultValues,
+    resolver: yupResolver(schema),
+  });
+
+  const submit = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.dialog}>
       <Typography className={styles.title} component="h2" variant="h3">
         Допоможіть нам відкрити світ книг для дітей!
       </Typography>
+      <form className={styles.form} onSubmit={handleSubmit(submit)}>
+        <Input
+          control={control}
+          name="donate"
+          label="Введіть або оберіть суму донату"
+          placeholder="Введіть суму донату"
+        />
+        <div className={styles['buttons-container']}>
+          <Button className={styles['button-template']} variant="outline">
+            50 грн
+          </Button>
+          <Button className={styles['button-template']} variant="outline">
+            100 грн
+          </Button>
+          <Button className={styles['button-template']} variant="outline">
+            200 грн
+          </Button>
+        </div>
+        <Button type="submit" color="secondary" className={styles['button-submit']}>
+          Підтримати зараз
+        </Button>
+      </form>
     </div>
   );
 };
