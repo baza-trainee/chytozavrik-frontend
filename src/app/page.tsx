6ffd369d8639c92a88b0d35d4e9da8689a69bbea
“Ruslan <russ_o@ukr.net>
2023-07-06 13:42:53 +0300

    ab-new1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import AdditionalInfo from '@/components/AdditionalInfo';
 import CtaBlock from '@/components/CtaBlock';
 import Hero from 'components/Hero';
 import Advantages from '@/components/Advantages';
+import About from '@/components/About/About';
+import AboutDetail from '@/components/AboutDetail/AboutDetail';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About/>
+      <AboutDetail/>
       <CtaBlock />
       <AdditionalInfo />
       <Advantages />
