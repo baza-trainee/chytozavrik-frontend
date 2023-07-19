26e370d72b8e235fd6a0cc581376c9a3732b974a
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-07-19 11:21:14 +0300

    CHANGE: Validation messages.
    ADD: NumberInput component.

--- src/components/common/form/index.ts
+++ src/components/common/form/index.ts
@@ -2,4 +2,5 @@ export { default as Input } from './Input';
 export * from './Input';
 export { default as PasswordInput } from './PasswordInput';
 export { default as Checkbox } from './Checkbox';
+export { default as NumberInput } from './NumberInput';
 export * from './validation';
