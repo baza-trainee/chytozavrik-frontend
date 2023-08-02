848fe22d192e508545973e24f121746cb7a0a0f6
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-08-02 20:03:47 +0300

    Styled ParentsProfile

--- src/components/ParentsProfile/ParentsProfile.tsx
+++ src/components/ParentsProfile/ParentsProfile.tsx
@@ -8,14 +8,13 @@ import wigwam from '../../../public/images/wigwam.svg';
 export default function ParentsProfile() {
     return (
         <section className={styles.section}>
-            <Container>
-                <Image src={avatar} alt ='аватарка батьків' width='106'/>
+            <Container className={styles.container}>
+                <Image src={avatar} className={styles.image} alt ='аватарка батьків' width='106'/>
                 <Button color='secondary' className={styles.button}>
                     <Image src={wigwam} width='24' alt='іконка вігваму' />
                 <Typography component="span" variant="h5">
                 Створити вігвам
                 </Typography>
-
                 </Button>
             </Container>    
         </section>
