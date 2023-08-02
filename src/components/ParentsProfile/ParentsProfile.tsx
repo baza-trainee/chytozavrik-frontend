e398eea5f24e9850e4be313b8077a351337857e2
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-08-02 17:54:32 +0300

    created component ParentsProfile

---
+++ src/components/ParentsProfile/ParentsProfile.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Container, Button, Typography} from 'components/common/'
+import Image from 'next/image';
+import styles from './ParentsProfile.module.scss';
+import avatar from '../../../public/images/avatar-parents.svg';
+import wigwam from '../../../public/images/wigwam.svg';
+
+export default function ParentsProfile() {
+    return (
+        <section className={styles.section}>
+            <Container>
+                <Image src={avatar} alt ='аватарка батьків' width='106'/>
+                <Button color='secondary' className={styles.button}>
+                    <Image src={wigwam} width='24' alt='іконка вігваму' />
+                <Typography component="span" variant="h5">
+                Створити вігвам
+                </Typography>
+
+                </Button>
+            </Container>    
+        </section>
+    )
+}
