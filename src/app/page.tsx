14c631b1edce3202d955486086ea0389c1feecc3
Andrii Zaimak <dev.andrii.zaimak@gmail.com>
2023-08-03 19:44:15 +0300

    UPDATE: header button.
    UPDATE: main page view when user is autorized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/config';
 import AdditionalInfo from '@/components/AdditionalInfo';
 import Donate from '@/components/Donate';
 import Hero from 'components/Hero';
-
 import Advantages from '@/components/Advantages';
 import About from '@/components/About/About';
 import AboutDetail from '@/components/AboutDetail/AboutDetail';
 import Partners from '@/components/Partners';
 import Auth from '@/components/Auth';
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <Hero />
-      <About />
-      <AboutDetail />
-      <Donate />
-      <Advantages />
-      <Partners />
-      <AdditionalInfo />
+      {session ? (
+        <>
+          <Hero />
+          <About />
+          <Advantages />
+          <Donate />
+          <Partners />
+          <AdditionalInfo />
+        </>
+      ) : (
+        <>
+          <Hero />
+          <About />
+          <AboutDetail />
+          <Donate />
+          <Advantages />
+          <Partners />
+          <AdditionalInfo />
+        </>
+      )}
+
       <Auth />
     </main>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,61 +1,30 @@
-'use client';
-
 import Image from 'next/image';
 import Link from 'next/link';
-import { useSession } from 'next-auth/react';
-import classNames from 'classnames';
-import { Route } from '@/constants';
-import { Button, Container, Typography } from '../common';
+import HeaderButton from './HeaderButton';
+import { Container, Typography } from '../common';
 import styles from './Header.module.scss';
-import { signOut } from 'next-auth/react';
 
 const Header = () => {
-  const session = useSession();
-
   return (
     <header>
-      <Container className={classNames(styles.header)}>
-        <div className={classNames(styles.headerContainer)}>
-          <Image
-            src="/header-logo.svg"
-            width={64}
-            height={54}
-            alt="Logo"
-            className={classNames(styles.logo)}
-          />
+      <Container className={styles.header}>
+        <div className={styles.headerContainer}>
+          <Image src="/header-logo.svg" width={64} height={54} alt="Logo" className={styles.logo} />
           <Image
             src="/chytozavryk.svg"
             width={105}
             height={14}
             alt="Logo"
-            className={classNames(styles.logoText)}
+            className={styles.logoText}
           />
         </div>
 
-        <div className={classNames(styles.buttonContainer)}>
-          <Typography className={classNames(styles.link)} component="h2" variant="h2">
+        <div className={styles.buttonContainer}>
+          <Typography className={styles.link} component="h2" variant="h2">
             <Link href="#">Про проєкт</Link>
           </Typography>
 
-          {session.status !== 'loading' &&
-            (session.status === 'authenticated' ? (
-              <Button
-                className={classNames(styles.button)}
-                variant="outline"
-                onClick={() => signOut({ callbackUrl: Route.HOME })}
-              >
-                Вихід
-              </Button>
-            ) : (
-              <Button
-                component="link"
-                href={Route.SIGN_IN}
-                className={classNames(styles.button)}
-                variant="outline"
-              >
-                Вхід
-              </Button>
-            ))}
+          <HeaderButton />
         </div>
       </Container>
     </header>
