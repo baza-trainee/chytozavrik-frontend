57adab9b0428f6fd75445c871612e4099b17e17a
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-08-05 22:30:02 +0300

    add env

---
+++ src/components/WigwamQuiz/WigwamQuiz.tsx
@@ -0,0 +1,11 @@
+import { Button } from '../common';
+
+const WigwamQuiz = () => {
+  return (
+    <div>
+      <Button>Почати вікторину</Button>
+    </div>
+  );
+};
+
+export default WigwamQuiz;
