e7d16abc4b7a7637032b6f54fd01b05b133161d0
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-08-09 15:33:12 +0300

    create quiz section

--- src/app/wigwam/page.tsx
+++ src/app/wigwam/page.tsx
@@ -2,12 +2,16 @@ import WigwamBooks from '@/components/WigwamBooks/WigwamBooks';
 import Container from '../../components/common/Container/Container';
 import WigwamReadBooks from '@/components/WigwamReadBooks/WigwamReadBooks';
 import styles from './wigwam.module.scss';
+import WigwamQuiz from '@/components/WigwamQuiz/WigwamQuiz';
 
 export default function Wigwam() {
   return (
     <main>
-      <Container className={styles.wraper}>
-        <WigwamReadBooks />
+      <Container className={styles.layout}>
+        <div className={styles.wraper}>
+          <WigwamReadBooks />
+          <WigwamQuiz />
+        </div>
         <WigwamBooks />
       </Container>
     </main>
