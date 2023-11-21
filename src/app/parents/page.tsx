5dcbeac3624c516d90b6e288195c265d10141679
RuslanDakeshev <bil08@outlook.com>
2023-11-22 00:51:05 +0100

    navbar

--- src/app/parents/page.tsx
+++ src/app/parents/page.tsx
@@ -1,53 +1,10 @@
-'use client';
 import React from 'react';
 import Parents from '@/components/Parents';
-import Navbar from '@/components/Navbar/Navbar';
-import NavbarMob from '@/components/NavbarMob/NavbarMob';
-import NavbarMobFooter from '../../components/NavbarMobFooter/NavbarMobFooter'
-
-import { useState, useEffect } from 'react';
 
 export default function ParentsPage() {
-  const [navBarVisible, setNavBarVisible] = useState(true);
-  const [headerVisible, setHeaderVisible] = useState(true);
-  const [footerVisible, setFooterVisible] = useState(true);
-
-  useEffect(() => {
-    function handleResize() {
-      if (window.innerWidth <= 767) {
-        setNavBarVisible(false)
-        setHeaderVisible(true);
-        setFooterVisible(true);
-        
-      } else {
-        setNavBarVisible(true)
-        setHeaderVisible(false);
-        setFooterVisible(false);
-        
-      }
-    }
-
-   
-    window.addEventListener('resize', handleResize);
-
-   
-    handleResize();
-
-    
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-  return <main>
-    {navBarVisible && <Navbar />}
-    
-     
-    
-    {headerVisible && <NavbarMob /> }
-    <Parents />
-   {footerVisible && <NavbarMobFooter /> }
-    
-  </main>;
-};
-
-
+  return (
+    <main>
+      <Parents />
+    </main>
+  );
+}
--- src/app/wigwam/[childId]/page.tsx
+++ src/app/wigwam/[childId]/page.tsx
@@ -1,19 +1,57 @@
+'use client';
 import WigwamBooks from '@/components/WigwamBooks/WigwamBooks';
 import Container from '../../../components/common/Container/Container';
 import WigwamReadBooks from '@/components/WigwamReadBooks/WigwamReadBooks';
 import styles from './wigwam.module.scss';
 import WigwamQuiz from '@/components/WigwamQuiz/WigwamQuiz';
+import NavbarMobFooter from '../../../components/NavbarMobFooter/NavbarMobFooter'
+import { useState, useEffect } from 'react';
+
+
 
 export default function Wigwam() {
-  return (
-    <main>
+
+
+
+  const [footerVisible, setFooterVisible] = useState(true);
+  
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 767) {
+       
+        setFooterVisible(true);
+        
+      } else {
+       
+        setFooterVisible(false);
+        
+      }
+    }
+
+   
+    window.addEventListener('resize', handleResize);
+
+   
+    handleResize();
+
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return <main>
+   
+     
       <Container className={styles.layout}>
         <div className={styles.wraper}>
           <WigwamReadBooks />
           <WigwamQuiz />
         </div>
         <WigwamBooks />
-      </Container>
+        </Container>
+      {footerVisible && <NavbarMobFooter />}
+     
     </main>
-  );
+  
 }
