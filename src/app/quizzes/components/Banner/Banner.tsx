10081c77f8ba71f078b6b17f3a84652acad8d116
Kateryna Laina <boba4ok@gmail.com>
2023-11-21 17:25:51 +0200

    feature: save commit

--- src/app/quizzes/components/Banner/Banner.tsx
+++ src/app/quizzes/components/Banner/Banner.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styles from './styles.module.scss'
 
 
+
 const Banner = () => {
   return (
-    <section>
+    <section className={styles.wrapper}>
       <div className={styles.banner}>
         <h2 className={styles.title}>Проходь вікторини та
           вигравай читозавриків</h2>
+        <div className={styles.icon}>
+          <img src='/images/UI/close.svg' alt='close icon' />
+        </div>
       </div>
     </section>
   );
