cf92781deda1a5a8afc0027d72811ef28bcb84df
Kateryna Laina <boba4ok@gmail.com>
2023-11-23 15:11:28 +0200

    feature: connect header to wigwam pages

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useConfetti } from './useConfetti';
 export * from './useFetch';
 export * from './useAuthAxiosInstance';
 export * from './useSignOut';
+export * from './useMedia';
