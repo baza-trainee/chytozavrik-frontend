de03e03386bc25e36cf74b5c905f9f6b0b52495e
Kateryna Laina <boba4ok@gmail.com>
2023-11-28 16:59:02 +0200

    feature: added admin button

--- src/app/(main)/components/Header/Header.tsx
+++ src/app/(main)/components/Header/Header.tsx
@@ -1,25 +1,45 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import HeaderButton from './HeaderButton';
-import { Container, Typography } from 'components/common';
+import { Button, Container } from 'components/common';
 import styles from './Header.module.scss';
+import { useSession } from 'next-auth/react';
+import * as process from 'process';
+import { useMedia } from '@/hooks';
+
 
 const Header = () => {
+  const { data: session } = useSession();
+  const {deviceType} = useMedia();
+
+  const adminData = process.env.NEXT_PUBLIC_ADMIN_EMAIL
+
   return (
     <header>
       <Container className={styles.header}>
-        <Link href="/" className={styles.headerContainer}>
-          <Image src="/images/logo/header-logo.svg" width={64} height={54} alt="Logo" className={styles.logo} />
+        <Link href='/' className={styles.headerContainer}>
+          <Image src='/images/logo/header-logo.svg' width={64} height={54} alt='Logo' className={styles.logo} />
           <Image
-            src="/images/logo/chytozavryk.svg"
+            src='/images/logo/chytozavryk.svg'
             width={105}
             height={14}
-            alt="Logo"
+            alt='Logo'
             className={styles.logoText}
           />
         </Link>
 
         <div className={styles.buttonContainer}>
+          {deviceType === "desktop"
+            && session?.user?.email === adminData
+            && <Button
+              className={styles.buttonAdmin}
+              component={'link'}
+              href={'admin'}
+              variant={'filled'}
+              color={'primary'}
+            >Адміністрування</Button>}
           <HeaderButton />
         </div>
       </Container>
