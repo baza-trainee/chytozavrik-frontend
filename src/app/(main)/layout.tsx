7507fc0648d28ad149eee0446df2dc520b8595e8
Kateryna Laina <boba4ok@gmail.com>
2023-12-02 11:03:07 +0200

    feature: gratitude modal

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next';
-import { Raleway, Montserrat_Alternates } from 'next/font/google';
 
 import '../globals.scss';
 import AppProvider from 'src/app/(main)/components/providers';
@@ -7,21 +6,6 @@ import Header from '@/app/(main)/components/Header';
 import Footer from 'components/Footer/Footer';
 import CookiesPanel from 'components/Cookies/CookiesPanel';
 
-const raleway = Raleway({
-  variable: '--raleway-font',
-  weight: ['300', '400', '500', '800'],
-  style: 'normal',
-  subsets: ['latin'],
-  display: 'swap',
-});
-
-const montserratAlternates = Montserrat_Alternates({
-  variable: '--montserrat-alternates-font',
-  weight: ['400', '600'],
-  style: 'normal',
-  subsets: ['latin'],
-  display: 'swap',
-});
 
 export const metadata: Metadata = {
   title: 'Читозаврик',
@@ -30,7 +14,6 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-
     <AppProvider>
       <Header />
       {children}
