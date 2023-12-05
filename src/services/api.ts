57657d8f8a0187e96b2eeda41971572760f5ac30
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2023-12-05 01:24:48 +0100

    Added functions for fetching data

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,15 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/config';
-import { AnswerType, FetchResponseType, QuizType, TokenType, UserType } from '@/types';
+import {
+  AnswerType,
+  FetchResponseType,
+  QuizInfoResponse,
+  AllQuizzesResponse,
+  TokenType,
+  UserType,
+  UsersQuizzesResponse,
+  QuizCategory,
+} from '@/types';
 
 const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL || '';
 
@@ -9,15 +18,37 @@ export const token: { access: string | null; refresh: string | null } = {
   refresh: null,
 };
 
+export const refreshTokenService = async (refreshToken: string) => {
+  try {
+    const response = await fetch(`${baseUrl}/auth/token/refresh/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ refresh: refreshToken }),
+    });
+    if (!response.ok) {
+      throw new Error(`Refresh token error: ${response.status}`);
+    }
+    const data = await response.json();
+    token.access = data.access;
+    token.refresh = refreshToken; // assuming the refresh token remains the same
+    return data;
+  } catch (error) {
+    console.error('Error in refreshTokenService:', error);
+    throw error;
+  }
+};
+
 export const privateFetch = async (
   input: RequestInfo | URL,
   options: RequestInit | undefined = undefined
 ) => {
-  const sesstion = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
   return await fetch(input, {
     headers: {
-      Authorization: 'Bearer ' + sesstion?.user.token.access,
+      Authorization: 'Bearer ' + session?.user.token.access,
     },
     ...options,
   });
@@ -77,9 +108,39 @@ export const getChildrenService = async () => {
   return await result.json();
 };
 
-export const getQuizByIdService = async (id: number): Promise<FetchResponseType<QuizType>> => {
+export const getQuizInfoByIdService = async (
+  id: number
+): Promise<FetchResponseType<QuizInfoResponse>> => {
   const result = await privateFetch(`${baseUrl}/quizzes/${id}`);
 
+  return await result.json();
+};
+
+export const getQuizzesService = async (
+  search: string = '',
+  page: string = '1',
+  page_size: number
+): Promise<FetchResponseType<AllQuizzesResponse>> => {
+  const result = await privateFetch(
+    `${baseUrl}/quizzes/?search=${search}&page=${page}&page_size=${page_size}`
+  );
+
+  return await result.json();
+};
+
+export const getUsersQuizzesService = async (
+  search: string = '',
+  page: string = '1',
+  category: QuizCategory = QuizCategory.All,
+  is_reversed: boolean = true,
+  page_size: number = 12,
+  childId: string
+): Promise<FetchResponseType<UsersQuizzesResponse>> => {
+  const selectedCategory = category ? `&${category}` : '';
+
+  const result = await privateFetch(
+    `${baseUrl}/users/me/children/${childId}/quizzes/?page=${page}&page_size=${page_size}&reverse=${is_reversed}&search=${search}${selectedCategory}`
+  );
   return await result.json();
 };
 
