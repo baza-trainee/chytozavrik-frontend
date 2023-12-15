02bbb2a50ecfff286581376de04da8c202abc076
Kateryna Laina <boba4ok@gmail.com>
2023-12-15 11:39:09 +0200

    fix: fix issue with displaying monsters

--- src/app/(wigwam)/components/header/NavbarMob/NavbarMob.tsx
+++ src/app/(wigwam)/components/header/NavbarMob/NavbarMob.tsx
@@ -8,23 +8,23 @@ import Link from 'next/link';
 import styles from './NavbarMob.module.scss';
 
 const NavbarMob = ({ avatar }: { avatar: string }) => (
-  <header className={styles.section}>
+  <section className={styles.section}>
     <Container>
       <div className={styles.wrapper}>
         <Image
-          src="/images/logo/header-logo.svg"
+          src='/images/logo/header-logo.svg'
           width={29}
           height={24}
-          alt="Logo"
+          alt='Logo'
           className={classNames(styles.log)}
         />
-        <Image src={avatar} width={32} height={32} alt="Logo" className={classNames(styles.log)} />
-        <Link href="/parents/lobby">
+        <Image src={avatar} width={32} height={32} alt='Logo' className={classNames(styles.log)} />
+        <Link href='/parents/lobby'>
           <LogOut width={32} height={32} className={classNames(styles.logo)} />
         </Link>
       </div>
     </Container>
-  </header>
+  </section>
 );
 
 export default NavbarMob;
