796fea97b2c405f903b96924e2e6f9f4fc32caa8
Kateryna Laina <boba4ok@gmail.com>
2023-12-20 22:34:26 +0200

    fix: fix issue with admincheckbox

--- src/app/(admin)/components/UI/AdminCheckBox/AdminCheckBox.tsx
+++ src/app/(admin)/components/UI/AdminCheckBox/AdminCheckBox.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import classNames from 'classnames';
-import styles from 'components/common/form/Checkbox/Checkbox.module.scss';
 import { Check } from 'lucide-react';
+import styles from 'components/common/form/Checkbox/Checkbox.module.scss';
 
 interface AdminCheckBoxProps {
   color?: 'primary' | 'secondary';
@@ -13,11 +13,11 @@ interface AdminCheckBoxProps {
 
 const AdminCheckBox = ({ color = 'primary', className, onChange, id }: AdminCheckBoxProps) => (
   <div className={classNames(className)}>
-    <label className={styles.label}>
-        <span className={classNames(styles['input-group'], styles[`input-group--${color}`])}>
-          <Check className={styles['input-checked-icon']} strokeWidth={4} />
-          <input className={styles.input} type="checkbox" onChange={onChange} />
-        </span>
+    <label className={styles.label} htmlFor={id.toString()}>
+      <span className={classNames(styles['input-group'], styles[`input-group--${color}`])}>
+        <input id={id.toString()} className={styles.input} type="checkbox" onChange={onChange} />
+        <Check className={styles['input-checked-icon']} strokeWidth={4} />
+      </span>
     </label>
   </div>
 );
