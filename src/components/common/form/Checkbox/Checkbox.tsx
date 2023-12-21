a408e7293febdc17041b3f27aabc13fee80dc4fa
Kateryna Laina <boba4ok@gmail.com>
2023-12-21 10:46:57 +0200

    fix: add id to checkbox

--- src/components/common/form/Checkbox/Checkbox.tsx
+++ src/components/common/form/Checkbox/Checkbox.tsx
@@ -8,33 +8,33 @@ import styles from './Checkbox.module.scss';
 
 export type InputProps<T extends FieldValues> = InputHTMLAttributes<HTMLInputElement> &
   UseControllerProps<T> & {
-    children?: ReactNode;
-    color?: 'primary' | 'secondary';
-  };
+  children?: ReactNode;
+  color?: 'primary' | 'secondary';
+};
 
 const Checkbox = <T extends FieldValues>({
-  name,
-  control,
-  color = 'primary',
-  className,
-  children,
-  ...props
-}: InputProps<T>) => {
+                                           name,
+                                           control,
+                                           color = 'primary',
+                                           className,
+                                           children,
+                                           ...props
+                                         }: InputProps<T>) => {
   const { field } = useController<T>({ name, control });
-const id = `checkbox-${name}`
+  const id = `checkbox-${name}`;
   return (
     <div className={classNames(className)}>
       <label className={styles.label} htmlFor={id}>
         <span className={classNames(styles['input-group'], styles[`input-group--${color}`])}>
-          <Check className={styles['input-checked-icon']} strokeWidth={4} />
           <input
             id={id}
-            type="checkbox"
+            type='checkbox'
             className={styles.input}
             {...field}
             checked={field.value}
             {...props}
           />
+          <Check className={styles['input-checked-icon']} strokeWidth={4} />
         </span>
         {children && <span className={styles['label-text']}>{children}</span>}
       </label>
