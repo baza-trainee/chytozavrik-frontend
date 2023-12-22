247dcb9d050c1addc015ec43e4afea6018149b10
Kateryna Laina <boba4ok@gmail.com>
2023-12-23 00:56:59 +0200

    feature: userInfo component

--- src/app/(admin)/admin/UserList/UserList.tsx
+++ src/app/(admin)/admin/UserList/UserList.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import styles from '@/app/(admin)/admin/Admin.module.scss';
+import UserItem from '@/app/(admin)/components/TableItems/UserItem/UserItem';
 import AdminHeader from '../../components/Header/AdminHeader';
 import TableHeader from '../../components/TableHeader/TableHeader';
-import UserItem from '@/app/(admin)/components/TableItems/UserItem/UserItem';
-import styles from '@/app/(admin)/admin/Admin.module.scss';
 
-const UserList = () => {
-  return (
-    <div className={styles.users}>
-      <AdminHeader withSearch withButton={false} withClose={false} heading="Користувачі" />
-      <div>
-        <TableHeader variant="users" colNames={['Email', 'Профілі дітей', 'Дата  реєстрації']} />
-        <UserItem/>
-      </div>
+const UserList = () => (
+  <div className={styles.users}>
+    <AdminHeader withSearch withButton={false} withClose={false} heading="Користувачі" />
+    <div>
+      <TableHeader variant="users" colNames={['Email', 'Профілі дітей', 'Дата  реєстрації']} />
+      <UserItem />
+      <UserItem />
     </div>
-  );
-};
+  </div>
+);
 
 export default UserList;
