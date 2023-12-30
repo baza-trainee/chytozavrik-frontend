5c3e04b37be4214fcacd5fd8a644737162f4364a
Kateryna Laina <boba4ok@gmail.com>
2023-12-30 18:22:36 +0200

    fix: fix error messages and message about pass

--- src/components/common/form/validation.ts
+++ src/components/common/form/validation.ts
@@ -12,18 +12,20 @@ export const notEmailMatch: yup.TestFunction<string | undefined, Record<string, 
 const emailRegex =
   // eslint-disable-next-line no-control-regex
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-const passworsRegex = /^(?=.*[0-9])(?=.*[a-zA-Z]).{6,30}$/;
-const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,64}/;
+const passworsRegex = /^(?=.*[a-zA-Z]).{6,30}$/;
 
 export const validation = {
   email: yup.string().lowercase().matches(emailRegex, 'Не вірна email адреса.').required(),
   signUpPassword: yup.string().required('Пароль не може бути порожнім.'),
   password: yup
     .string()
-    .matches(
-      passwordRegex,
-      'Пароль від 8 до 64 символів, містить латинські літери, принаймні одну велику та маленьку літеру, цифру та спеціальний символ (@#$%^&+=!).'
-    )
+    .min(8, ({ min }) => `Пароль має бути ${min} символів.`)
+    .max(64, ({ max }) => `Пароль має бути не більше ніж ${max} символів.`)
+    .matches(passworsRegex, 'Пароль повинен бути латиницею.')
+    .matches(/[0-9]/, 'Пароль повинен містити хоча б 1 цифру.')
+    .matches(/[A-Z]/, 'Пароль повинен містити хоча б одну велику літеру.')
+    .matches(/[a-z]/, 'Пароль повинен містити хоча б одну маленьку літеру')
+    .matches(/[@#$%^&+=!]/, 'Пароль повинен містити хоча б один символ з перелічених: "@#$%^&+=!".')
     .test('notEmailMatch', 'Пароль надто схожий на email', notEmailMatch)
     .required('Пароль не може бути порожнім.'),
   confirmPassword: yup
