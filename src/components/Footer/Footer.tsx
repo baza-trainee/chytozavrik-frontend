c59411ef2e2e754b9eef6fdda988cad88978fa2a
Kateryna Laina <boba4ok@gmail.com>
2024-02-02 14:03:46 +0200

    feat: added favicon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,30 +11,39 @@ import { formatPhoneNumber } from '@/utils/formatPhoneNumber';
 import Container from '../common/Container/Container';
 import Typography from '../common/Typography/Typography';
 import styles from './Footer.module.scss';
+import { useGetDocuments } from '@/hooks/useGetDocuments';
+import { Fragment } from 'react';
 
+interface IDocument {
+  file:string,
+  name: string,
+  id: number,
+  updated_at: string
+}
 const Footer = () => {
   const pathName = usePathname();
   const isShowFooter = !pathName.includes(Route.WIGWAM);
   const { contacts } = useQueryContactInfo();
+  const { documents } = useGetDocuments();
 
   return (
     isShowFooter && (
       <footer className={styles.footer}>
         <Container className={styles.container}>
           <div className={styles.footerContainer}>
-            <Link href="/" className={styles.logoContainer}>
+            <Link href='/' className={styles.logoContainer}>
               <Image
-                src="/images/logo/logo-footer.svg"
+                src='/images/logo/logo-footer.svg'
                 width={58}
                 height={50}
-                alt="Logo"
+                alt='Logo'
                 className={styles.logo}
               />
               <Image
-                src="/images/logo/chytozavryk-footer.svg"
+                src='/images/logo/chytozavryk-footer.svg'
                 width={120}
                 height={16}
-                alt="Logo"
+                alt='Logo'
                 className={styles.logoText}
               />
             </Link>
@@ -42,39 +51,33 @@ const Footer = () => {
             <div className={styles.wrapper}>
               <div className={classNames(styles.list, styles.informationText, styles.footerList)}>
                 <ul className={styles.class4}>
-                  <Typography className={styles.informationText} component="p" variant="footer">
-                    <Link href="https://baza-trainee.tech">Про проєкт</Link>
+                  <Typography className={styles.informationText} component='p' variant='footer'>
+                    <Link href='https://baza-trainee.tech'>Про проєкт</Link>
                   </Typography>
-                  <Typography className={styles.informationText} component="p" variant="footer">
+                  <Typography className={styles.informationText} component='p' variant='footer'>
                     <Link href={Route.WIGWAM_LOBBY}>До вігваму</Link>
                   </Typography>
                 </ul>
                 <ul className={styles.class1}>
-                  <Typography className={styles.informationText} component="p" variant="footer">
-                    <Link
-                      href="/pdf/privacy policy.pdf#toolbar=0"
-                      target="_blank"
-                      className={styles.linkText}
-                    >
-                      Політика конфіденційності
-                    </Link>
-                  </Typography>
-
-                  <Typography className={styles.informationText} component="p" variant="footer">
-                    <Link
-                      href="/pdf/site-rules.pdf#toolbar=0"
-                      target="_blank"
-                      className={styles.linkText}
-                    >
-                      Правила користування сайтом
-                    </Link>
-                  </Typography>
+                  {documents?.map((document : IDocument) =>
+                  <Fragment key={document.id}>
+                    <Typography className={styles.informationText} component='p' variant='footer'>
+                      <Link
+                        href={`${document.file}#toolbar=0`}
+                        target='_blank'
+                        className={styles.linkText}
+                      >
+                        {document.name}
+                      </Link>
+                    </Typography>
+                  </Fragment>
+                  )}
                 </ul>
               </div>
 
               <div className={classNames(styles.list, styles.contacts)}>
                 <ul>
-                  <Typography className={styles.informationContact} component="p" variant="h6">
+                  <Typography className={styles.informationContact} component='p' variant='h6'>
                     <Phone width={18} height={18} className={styles.image} />
                     {contacts && formatPhoneNumber(contacts.first_phone)}
                   </Typography>
@@ -82,8 +85,8 @@ const Footer = () => {
                   {contacts && contacts.second_phone && (
                     <Typography
                       className={styles.informationContactSecond}
-                      component="p"
-                      variant="h6"
+                      component='p'
+                      variant='h6'
                     >
                       <Phone width={18} height={18} className={styles.image} />
                       {contacts && formatPhoneNumber(contacts.second_phone)}
@@ -92,8 +95,8 @@ const Footer = () => {
                   <a href={`mailto:${contacts && contacts.email}`}>
                     <Typography
                       className={styles.informationMail}
-                      component="p"
-                      variant="footer-mail"
+                      component='p'
+                      variant='footer-mail'
                     >
                       <Mail width={18} height={18} className={styles.image} />
                       {contacts && contacts.email}
@@ -104,7 +107,7 @@ const Footer = () => {
             </div>
           </div>
 
-          <Typography className={styles.footerText} component="p" variant="footer-end">
+          <Typography className={styles.footerText} component='p' variant='footer-end'>
             Розробка Baza Trainee Ukraine 2024 Усі права захищені
           </Typography>
         </Container>
