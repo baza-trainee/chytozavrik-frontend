14a4660ef6fea78f886a51c2521113794f714d95
Kateryna Laina <boba4ok@gmail.com>
2024-02-03 11:45:14 +0200

    fix: fix problem with routing to last quiz

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,30 +9,6 @@ export const metadata: Metadata = {
   title: 'Інтерактивна Вікторина Для Маленьких Читолюбів - Читозаврик',
   description:
     'Додаток розроблений спеціально для дітей віком від 6 до 9 років. Захоплюючі книжкові вікторини дозволяють не тільки закріпити знання про прочитане, а ще й розвивати розумові навички.',
-  icons: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      url: '/favicon/favicon-32X32.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/x-icon',
-      url: '/favicon/favicon.ico',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      url: '/favicon/favicon-16X16.png',
-    },
-    {
-      rel: 'apple-touch-icon',
-      sizes: '180x180',
-      url: '/favicon/apple-touch-icon.png',
-    },
-  ],
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
